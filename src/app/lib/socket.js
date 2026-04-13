7f9de21360ef2dd4bdc4bd221fc45a9edd980df6
Lakshay7118 <lakshyamehra321@gmail.com>
2026-04-13 14:36:30 +0530

    fix

--- src/app/lib/socket.js
+++ src/app/lib/socket.js
@@ -3,11 +3,9 @@
 import { io } from "socket.io-client";
 
 // ✅ Use env variable (Vercel) OR fallback to Railway
-const BASE =
+const BACKEND =
   process.env.NEXT_PUBLIC_BACKEND ||
   "https://whatsapp-backend-production-308a.up.railway.app";
-
-const API_BASE = `${BASE}/api`;
 
 let socket = null;
 
